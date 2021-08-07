cb191b91b8a87a0acc516998bd53ceb36fbec381
Patric Sanchez <patrics27@outlook.com>
2021-08-07 15:18:38 -0700

    Edits on the Python File

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,6 +1,10 @@
-import random
+import random  #Imports Random Python Library
 
-userChoice = input("What is your move. 'r' for Rock, 'p' for Paper, or 's' for Scissors? ")
-userChoice = userChoice.lower()
+userChoice = input("What is your move. 'r' for Rock, 'p' for Paper, or 's' for Scissors? ") #Asks the user for their move
+userChoice = userChoice.lower() #Convert the user's input to lowercase to easily compare to the computer's choice
 
-print("You chose: " + userChoice)
+print("You chose: " + userChoice) #Prints what the user chose
+
+computerChoice = random.choice(['r', 'p', 's']) #Randomly generate's the computer's choice
+
+print(" The compouter chose: " + computerChoice) #Prints the computer's choice
